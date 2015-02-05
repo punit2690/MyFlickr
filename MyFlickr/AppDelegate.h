c8905ee3b51f2d98b55a8ac7a15b3cf6de3b0cec
Punit Kulkarni <punitkulkarni@Punits-MacBook-Air.local>
2015-02-05 08:22:26 -0500

    Initial Commit

---
+++ MyFlickr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFlickr
+//
+//  Created by Punit Kulkarni on 2/5/15.
+//  Copyright (c) 2015 Punit Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFlickr
+//
+//  Created by Punit Kulkarni on 2/5/15.
+//  Copyright (c) 2015 Punit Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
